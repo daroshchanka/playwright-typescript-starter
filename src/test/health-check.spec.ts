b613dcb3dac14a508c1d4937a2a605ed8fba8178
Max Daroshchanka <daroshchanka@gmail.com>
2024-09-24 00:20:39 +0300

    typos: yeat another fix (should setup some linter for vs code :))

--- src/test/health-check.spec.ts
+++ src/test/health-check.spec.ts
@@ -3,9 +3,9 @@ import { LoggerProvider } from '../project/loggerProvider';
 
 const log = LoggerProvider.logger;
 
-test.describe('health-check Playright Starter', () => {
+test.describe('health-check Playwright Starter', () => {
 
-    test('playright API context OK', async ({ request }) => {
+    test('playwright API context OK', async ({ request }) => {
         let response = await request.get('https://httpbin.org/get');
         expect(response.status()).toBe(200);
         expect((await response.json())['headers']['User-Agent'].includes('Mozilla/5.0')).toBeTruthy();
@@ -13,7 +13,7 @@ test.describe('health-check Playright Starter', () => {
 
     });
 
-    test('playright WEB context OK', async ({ page }) => {
+    test('playwright WEB context OK', async ({ page }) => {
         await page.goto('https://todomvc.com/examples/react/dist/')
         expect(page.url()).toContain('https://todomvc.com/examples/react/dist/');
         log.info('info log WEB');
